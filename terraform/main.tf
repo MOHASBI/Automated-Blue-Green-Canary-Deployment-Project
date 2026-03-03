459dbe1be2c8f4c889aa4686ab07d35121556773
hasbi <mohhas048@gmail.com>
2026-03-03 10:03:50 +0000

    ADD: Import ALB module

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,19 @@ module "acm" {
   private_zone       = var.private_zone
   cert_validation_ttl = var.cert_validation_ttl
 }
+
+module "alb" {
+  source = "./modules/ALB"
+
+  vpc_id            = module.vpc.vpc_id
+  public_subnets_id = module.vpc.public_subnets_id
+  acm_certificate_arn = module.acm.acm_certificate_arn
+  target_port       = var.target_port
+  health_check_path = var.health_check_path
+  health_check_matcher = var.health_check_matcher
+  ssl_policy        = var.ssl_policy
+  zone_name         = var.zone_name
+  record_name       = var.record_name
+  record_type       = var.record_type
+  load_balancer_type = var.load_balancer_type
+}
