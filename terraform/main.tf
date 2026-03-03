41701571f16ad96b237d99d9ac7e9d65bfbde83c
hasbi <mohhas048@gmail.com>
2026-03-03 09:56:51 +0000

    ADD: Import ACM module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,12 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   availability_zones  = var.availability_zones
 }
+
+module "acm" {
+  source = "./modules/ACM"
+
+  domain_name        = var.domain_name
+  hosted_zone_name   = var.zone_name
+  private_zone       = var.private_zone
+  cert_validation_ttl = var.cert_validation_ttl
+}
