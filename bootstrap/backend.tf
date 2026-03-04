95c09e27297207b78812a23a851176ddfc04972b
hasbi <mohhas048@gmail.com>
2026-03-04 17:21:01 +0000

    Align backend config and migrate state

--- bootstrap/backend.tf
+++ bootstrap/backend.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_version = ">= 1.6.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+terraform {
   backend "s3" {
     bucket         = "blue-green-848153448908-tf-state"
     key            = "Bootstrap/terraform.tfstate"
