fdaace5ec581514adce1d565f20f64ac510dccae
hasbi <mohhas048@gmail.com>
2026-03-04 16:27:43 +0000

    FIX: CI/CD Implementation. ACM change added

--- terraform/modules/ACM/main.tf
+++ terraform/modules/ACM/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
