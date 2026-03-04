af2da04ae39fe1e32b8622b72620cc19f81066cd
hasbi <mohhas048@gmail.com>
2026-03-04 19:43:55 +0000

    UPDATE: domain name variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -80,7 +80,7 @@ variable "zone_name" {
 
 variable "record_name" {
   type    = string
-  default = "AsterixHealth"
+  default = "tm"
 }
 
 variable "record_type" {
@@ -95,7 +95,7 @@ variable "load_balancer_type" {
 
 variable "domain_name" {
   type    = string
-  default = "AsterixHealth.mohamedhassanproject.com"
+  default = "tm.mohamedhassanproject.com"
 }
 
 variable "private_zone" {
